ce433d40d3fec690b2dd9dad05f7966195afadd7
daniel21dev <daniel21dev@gmail.com>
2021-06-08 15:52:36 -0500

    added typeorm config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import "reflect-metadata";
 import { startServer } from "./app";
+import { connect } from "./config/typeorm";
 
 async function main() {
   const app = await startServer();
   app.listen(4000);
+  connect();
   console.log("server on port", 4000);
 }
 
